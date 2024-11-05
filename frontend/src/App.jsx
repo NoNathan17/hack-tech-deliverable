3c9f048d2d7674446b2e12a1131382ac95b81b4e
NoNathan17 <nathanong1217@gmail.com>
2024-11-05 15:09:30 -0800

    Functionality to show quotes onto the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,28 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+function Quote({ name, message, time }) {
+    return (
+        <div className="quote">
+            <p><strong>{name}</strong></p>
+            <p>{message}</p>
+            <p><em>{new Date(time).toLocaleString()}</em></p>
+        </div>
+    );
+}
+
 function App() {
+	const [quotes, setQuotes] = useState([])
+
+	useEffect(() => {
+		async function fetchQuotes() {
+			const response = await fetch("/api/quotes?max_age=all");
+            const data = await response.json();
+            setQuotes(data);
+		}
+		fetchQuotes();
+	}, []);
+
 	return (
 		<div className="App">
 			{/* TODO: include an icon for the quote book */}
@@ -19,9 +41,14 @@ function App() {
 			<h2>Previous Quotes</h2>
 			{/* TODO: Display the actual quotes from the database */}
 			<div className="messages">
-				<p>Peter Anteater</p>
-				<p>Zot Zot Zot!</p>
-				<p>Every day</p>
+				{quotes.map((quote, index) => (
+                    <Quote
+                        key={index}
+                        name={quote.name}
+                        message={quote.message}
+                        time={quote.time}
+                    />
+                ))}
 			</div>
 		</div>
 	);
